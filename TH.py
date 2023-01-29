85a71177d9703dc03b0318b84b6fc9513e19edcd
Ashutosh.shukla <113657895+shukla-ashutosh4@users.noreply.github.com>
2023-01-29 23:35:53 +0530

    Create TH.py

---
+++ TH.py
@@ -0,0 +1,23 @@
+print("Welcome to the Treasure Island.\nYour Mission is to find the Treasure.")
+print ("Left or Right")
+X = input()
+if X == "Right":
+    print("Move")
+else:
+    print("K.O.")
+print("Swim Or Wait")
+Y = input()
+if Y == "Wait":
+    print("LETSS GO!!!")
+else:
+    print("K.O.")
+print("Which COLOUR Door? ")
+Z = input()
+if Z == "Red":
+    print("YOU WIN!!!!!!")
+else:
+    print("YOU LOSE")
+    
+    
+    
+    
