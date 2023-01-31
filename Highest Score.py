cb0c6b361ed28024d7819edea65ec910c85372c8
shukla-ashutosh4 <113657895+shukla-ashutosh4@users.noreply.github.com>
2023-01-31 23:43:11 +0530

    Update and rename Heighest Score.py to Highest Score.py

--- Highest Score.py
+++ Highest Score.py
@@ -3,9 +3,9 @@ for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print (student_scores)
 
-heighest_score = 0
+highest_score = 0
 for score in student_scores:
-    if score > heighest_score:
-        heighest_score = score
-print(f"The highest score in the class is: {heighest_score}")
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}")
     
