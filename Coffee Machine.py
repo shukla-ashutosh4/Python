d56bd429beb812160e38f536167b9476d18924b1
Ashutosh Shukla <113657895+shukla-ashutosh4@users.noreply.github.com>
2023-08-02 01:22:24 +0530

    Add files via upload

---
+++ Coffee Machine.py
@@ -0,0 +1,93 @@
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 23,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 27,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 33,
+    }
+}
+profit = 0
+resources = {
+    "water": 1000,
+    "milk": 1000,
+    "coffee": 100,
+}
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted"""
+    print("Please insert coins.")
+    total = int(input("How many ₹1 coins?: ")) * 1
+    total += int(input("How many ₹2 coins?: ")) * 2
+    total += int(input("How many ₹5 coins?: ")) * 5
+    total += int(input("How many ₹10 coins?: ")) * 10
+    total += int(input("How many ₹20 coins?: ")) * 20
+    return total
+
+def is_transaction_successful(money_recieved, drink_cost):
+    """Return True when payment is accepted, or False if money is insufficient"""
+    if money_recieved >= drink_cost:
+        change = (money_recieved - drink_cost)
+        print(f"Here is ₹{change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+        
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕. Enjoy!")
+
+
+
+is_on = True
+
+
+while is_on:
+    choice = input("What would you like? (espresso(₹23/-)/latte(₹27/-)/cappuccino(₹33/-)): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"water: {resources['water']}ml")
+        print(f"milk: {resources['milk']} ml")
+        print(f"coffee: {resources['coffee']} gm") 
+        print(f"money: ₹{profit}")
+    else:
+        drink = MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
+
+
+
